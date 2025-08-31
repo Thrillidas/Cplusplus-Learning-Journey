a91299d91217df22aade36586cca114cf515ad92
Thrillidas <franciscobrucesubiabre@gmail.com>
2025-08-30 21:39:51 -0400

    Hello world

---
+++ cpp2-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+//I'm a comment !
+
+int main()
+{
+    using namespace std;
+    cout << "Hello word";
+    cout << endl;
+    cout << "You'll regret learn C++ ! Muahahahaha" <<endl;
+    return 0;
+}
