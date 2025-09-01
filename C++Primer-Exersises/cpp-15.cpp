dea9fb8b08adcda978eefb2b93fae37862067b30
Thrillidas <franciscobrucesubiabre@gmail.com>
2025-09-01 00:10:30 -0400

    Exersise 1.15 submited

---
+++ C++Primer-Exersises/cpp-15.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main ()
+{
+    //Syntax error
+    int val = "Papaya"; // String is in int variable
+
+    std::cout << Hola, como estas; // it needs ""
+    cout << va; //It need to be reference to std library. The variable is bad written.
+
+
+    return 0 //it need semi-colon
+}
