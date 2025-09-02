2de93e66a07b439559350acb266202903a9e04d2
Thrillidas <franciscobrucesubiabre@gmail.com>
2025-09-01 22:00:13 -0400

    Exercise 1.16 submited

---
+++ C++Primer-Exersises/cpp-16.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main()
+{
+    int value = 0;
+    int sum = 0;
+    
+    while(std::cin >> value)
+    {
+        sum = sum + value;
+        std::cout << sum;
+    }
+    return 0;
+}
