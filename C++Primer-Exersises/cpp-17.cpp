ad50b3c1e4136d1005b6924d53452bd5ad2d4345
Thrillidas <franciscobrucesubiabre@gmail.com>
2025-09-01 23:16:54 -0400

    Excersise 1.17 submited

---
+++ C++Primer-Exersises/cpp-17.cpp
@@ -0,0 +1,30 @@
+//If there's no duplicate values, the program wont print the counting, until comes a new value
+
+
+#include <iostream>
+
+int main()
+{
+    int currVal = 0, val = 0;
+    if (std::cin >> currVal)
+    {
+        int cnt = 1;
+        while(std::cin >> val)
+        {
+            if(val == currVal)
+            {
+                ++cnt;
+            }
+            else
+            {
+                std::cout << currVal << " Ocurrs "
+                          << cnt << " times" <<std::endl;
+                currVal = val ;
+                cnt = 1;
+            }
+        }
+        std::cout << currVal << " Ocurrs "
+                  << cnt << " times " << std::endl;
+    } 
+    return 0;
+}
